d0443d76228e59ad3c29a71c23a334cdb40ab9c4
john Meng <john.meng014@gmail.com>
2019-09-26 13:16:20 -0400

    was able to get the information stored onto firebase

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-  var config = {
-    apiKey: "AIzaSyBBRdxMh1gGh89gN6b2CYvN5j10zDsws4w",
-    authDomain: "traintrain-70217.firebaseapp.com",
-    databaseURL: "https://traintrain-70217.firebaseio.com",
-    projectId: "traintrain-70217",
-    storageBucket: "",
-    messagingSenderId: "729956879324",
-    appId: "1:729956879324:web:e0dbac2ecb56ec6e171d19",
-    measurementId: "G-KNGM9XG00J"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBZJv6c0e1xnl4UUjhcbzsSBfcaj-l0Ons",
+  authDomain: "first-database-eaa8a.firebaseapp.com",
+  databaseURL: "https://first-database-eaa8a.firebaseio.com",
+  projectId: "first-database-eaa8a",
+  storageBucket: "",
+  messagingSenderId: "829727741676",
+  appId: "1:829727741676:web:eb6cf5ba6f6a03ea5737c9"
+};
 
-  var database = firebase.database();
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
   // function to add train time
   $("#add-train-btn").on("click", function(event){
@@ -23,4 +22,25 @@
     var trainStart = moment($("#start-input").val().trim(), "H HH").format("LT");
     var trainFrequency = $("#frequency-input").val().trim();
 
-  })
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      start: trainStart,
+      frequency: trainFrequency,
+
+    };
+    //uploads data to database
+    database.ref().push(newTrain);
+
+    console.log(newTrain.trainName);
+    console.log(newTrain.trainDestination);
+    console.log(newTrain.trainStart);
+    console.log(newTrain.trainFrequency);
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+
+    
+  });
